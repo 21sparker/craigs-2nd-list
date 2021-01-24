057b02e643ccb23023123751710406fc49c5524e
21sparker <seanparker500@gmail.com>
2021-01-24 14:44:18 -0500

    Category list all results working

--- src/api/category/CategoryService.ts
+++ src/api/category/CategoryService.ts
@@ -22,7 +22,7 @@ class CategoryService {
         return CategoryService.instance;
     }
 
-    public async listAll(): Promise<Array<Category>> {
+    public async listAll(): Promise<CategoryInterface[]> {
         const queryResults = await Category.query()
             .join('subcategories', 'categories.category_id', '=', 'subcategories.category_id')
             .select('categories.category_id',
@@ -31,29 +31,35 @@ class CategoryService {
                 'subcategories.name AS subcategory'
             );
 
-        let output: { [key: string]: [] } = {};
+        let output: CategoryInterface[] = [];
+        let currentCategory: string;
+        let currentCategoryInterface: CategoryInterface;
+        queryResults.forEach((result: Category) => {
 
-        const results = queryResults.map((result: Category) => {
-            const categoryName: string = result.name;
-            if (output.hasOwnProperty(categoryName)) {
-                output[categoryName] = 
+            // Change result to any type because we aliased some column names
+            // that aren't part of the Category type.
+            const item = result as any;
+
+            // We are assuming categories are grouped together so create a new
+            // CategoryInterface object when we find a new one as we iterate
+            if (item.name !== currentCategory) {
+                currentCategoryInterface = {
+                    category_id: item.category_id,
+                    name: item.name,
+                    subcategories: []
+                }
+                output.push(currentCategoryInterface);
+                currentCategory = item.name;
             }
 
-            // const item: CategoryQueryOuput = result as unknown as CategoryQueryOuput;
-            
-            // if (!output.hasOwnProperty(item.category) {
-            //     output[item.category] = [];
-            // }
+            // Add the subcategory to the category interface
+            currentCategoryInterface.subcategories.push({
+                subcategory_id: item['subcategory_id'] as number,
+                name: item['subcategory'] as string,
+            });
         });
-        // let a = results.map(item => { category: item.category, })
-        // // const results = await Category.query().select();
-        // console.log(typeof results[0])
-        // const output = {};    
-        // results.forEach((result) => {
-        //     if (!output.hasOwnProperty(result.category))
-        // })
 
-        return queryResults;
+        return output;
     }
 }
 
