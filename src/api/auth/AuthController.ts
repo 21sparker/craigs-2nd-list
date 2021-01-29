0dbe3c6b6a136956d14de2a38640fb3b9731df63
21sparker <seanparker500@gmail.com>
2021-01-28 19:53:47 -0500

    Add 1h exp to token

--- src/api/auth/AuthController.ts
+++ src/api/auth/AuthController.ts
@@ -19,7 +19,8 @@ class AuthController {
             const token = jwt.sign({
                 user_id: user.user_id,
                 email: user.email
-            }, AuthConfig.privateKey);
+            }, AuthConfig.privateKey,
+            { expiresIn: '1h' });
             res.status(200).json({token: token});
         } catch(err) {
             res.status(500).end();
